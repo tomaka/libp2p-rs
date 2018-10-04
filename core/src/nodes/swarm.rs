bdcbdf9081f7540e0ee4f1c9627530d542a4414c
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-10-04 10:50:24 +0200

    More work on new Swarm

--- core/src/nodes/swarm.rs
+++ core/src/nodes/swarm.rs
@@ -19,10 +19,13 @@
 // DEALINGS IN THE SOFTWARE.
 
 use futures::prelude::*;
+use muxing::StreamMuxer;
+use nodes::handled_node::NodeHandler;
+use nodes::node::Substream;
 use nodes::protocols_handler::{NodeHandlerWrapper, ProtocolsHandler};
 use nodes::raw_swarm::{RawSwarm, RawSwarmEvent, ConnectedPoint, HandlerFactory};
 use std::{io, marker::PhantomData};
-use {Multiaddr, Transport};
+use {ConnectionUpgrade, Multiaddr, PeerId, Transport};
 
 /// Contains the state of the network, plus the way it should behave.
 pub struct Swarm<TBehaviour>
@@ -39,7 +42,8 @@ where TBehaviour: NetworkBehavior,
     behaviour: TBehaviour,
 }
 
-struct LocalBuilder<TProtocolsHandler>(PhantomData<TProtocolsHandler>);
+// TODO: document
+pub struct LocalBuilder<TProtocolsHandler>(PhantomData<TProtocolsHandler>);
 impl<TProtocolsHandler> HandlerFactory for LocalBuilder<TProtocolsHandler>
 where TProtocolsHandler: ProtocolsHandler + Default,
 {
@@ -49,9 +53,23 @@ where TProtocolsHandler: ProtocolsHandler + Default,
     }
 }
 
-impl<TBehaviour> Swarm<TBehaviour>
+impl<TBehaviour, TMuxer> Swarm<TBehaviour>
 where TBehaviour: NetworkBehavior,
-      TBehaviour::ProtocolsHandler: Default,        // TODO: this is a hack because of the builder
+      TMuxer: StreamMuxer + Send + Sync + 'static,
+      <TMuxer as StreamMuxer>::OutboundSubstream: Send + 'static,
+      <TMuxer as StreamMuxer>::Substream: Send + 'static,
+      TBehaviour::Transport: Transport<Output = (PeerId, TMuxer)> + Clone,
+      <TBehaviour::Transport as Transport>::Dial: Send + 'static,
+      <TBehaviour::Transport as Transport>::Listener: Send + 'static,
+      <TBehaviour::Transport as Transport>::ListenerUpgrade: Send + 'static,
+      TBehaviour::ProtocolsHandler: ProtocolsHandler<Substream = Substream<TMuxer>> + Send + 'static + Default,        // TODO: Default is a hack because of the builder
+      <TBehaviour::ProtocolsHandler as ProtocolsHandler>::InEvent: Send + 'static,
+      <TBehaviour::ProtocolsHandler as ProtocolsHandler>::OutEvent: Send + 'static,
+      <TBehaviour::ProtocolsHandler as ProtocolsHandler>::Protocol: ConnectionUpgrade<Substream<TMuxer>> + Send + 'static,
+      <<TBehaviour::ProtocolsHandler as ProtocolsHandler>::Protocol as ConnectionUpgrade<Substream<TMuxer>>>::Future: Send + 'static,
+      <<TBehaviour::ProtocolsHandler as ProtocolsHandler>::Protocol as ConnectionUpgrade<Substream<TMuxer>>>::NamesIter: Clone + Send + 'static,
+      <<TBehaviour::ProtocolsHandler as ProtocolsHandler>::Protocol as ConnectionUpgrade<Substream<TMuxer>>>::UpgradeIdentifier: Send + 'static,
+      <NodeHandlerWrapper<TBehaviour::ProtocolsHandler> as NodeHandler>::OutboundOpenInfo: Send + 'static, // TODO: shouldn't be necessary
 {
     /// Returns the transport passed when building this object.
     #[inline]
@@ -62,7 +80,7 @@ where TBehaviour: NetworkBehavior,
     /// Returns an iterator that produces the list of addresses we're listening on.
     #[inline]
     pub fn listeners(&self) -> impl Iterator<Item = &Multiaddr> {
-        self.raw_swarm.listeners()
+        RawSwarm::listeners(&self.raw_swarm)
     }
 
     /// Returns a reference to the network behaviour.
@@ -85,14 +103,14 @@ where TBehaviour: NetworkBehavior,
         loop {
             match self.raw_swarm.poll() {
                 Async::Ready(Some(event)) => {
-                    self.behaviour.inject_event(&event, &mut self.raw_swarm.limited_capabilities())
+                    self.behaviour.inject_event(&event, &mut self.raw_swarm)
                 },
                 Async::Ready(None) => unreachable!(),       // TODO:
                 Async::NotReady => break,
             }
         }
 
-        self.behaviour.poll(&mut self.raw_swarm.limited_capabilities())
+        self.behaviour.poll(&mut self.raw_swarm)
     }
 }
 
@@ -115,10 +133,22 @@ pub trait NetworkBehavior {
     fn inject_event(
         &mut self,
         ev: &RawSwarmEvent<Self::Transport, <Self::ProtocolsHandler as ProtocolsHandler>::OutEvent>,
-        raw_swarm: &mut RawSwarmLimitedControls
+        raw_swarm: &mut RawSwarm<
+            Self::Transport,
+            <Self::ProtocolsHandler as ProtocolsHandler>::InEvent,
+            <Self::ProtocolsHandler as ProtocolsHandler>::OutEvent,
+            LocalBuilder<Self::ProtocolsHandler>
+        >,
     );
 
     /// Polls for final events.
-    fn poll(&mut self, raw_swarm: &mut RawSwarmLimitedControls)
-        -> Poll<Option<Self::OutEvent>, io::Error>;
+    fn poll(
+        &mut self,
+        raw_swarm: &mut RawSwarm<
+            Self::Transport,
+            <Self::ProtocolsHandler as ProtocolsHandler>::InEvent,
+            <Self::ProtocolsHandler as ProtocolsHandler>::OutEvent,
+            LocalBuilder<Self::ProtocolsHandler>
+        >
+    ) -> Poll<Option<Self::OutEvent>, io::Error>;
 }
