f74061d32e20f6a8a3dd80439e6dd9bff28db2f3
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-01-03 14:28:44 +0100

    Add reference raw benchmarks

--- muxers/mplex/benches/test.rs
+++ muxers/mplex/benches/test.rs
@@ -29,7 +29,7 @@ use tokio::{
     runtime::current_thread::Runtime
 };
 
-fn connect_and_send_data(bench: &mut test::Bencher, data: &[u8]) {
+fn mplex_connect_and_send_data(bench: &mut test::Bencher, data: &[u8]) {
     let transport =
         libp2p_tcp::TcpConfig::new().with_upgrade(libp2p_mplex::MplexConfig::new());
 
@@ -74,25 +74,90 @@ fn connect_and_send_data(bench: &mut test::Bencher, data: &[u8]) {
     })
 }
 
-#[bench]
-fn connect_and_send_hello(bench: &mut test::Bencher) {
-    connect_and_send_data(bench, b"hello world")
+fn raw_tcp_connect_and_send_data(bench: &mut test::Bencher, data: &[u8]) {
+    let transport = libp2p_tcp::TcpConfig::new();
+
+    bench.iter(move || {
+        let (listener, addr) = transport
+            .clone()
+            .listen_on("/ip4/127.0.0.1/tcp/0".parse().unwrap())
+            .unwrap();
+
+        let listener_side = listener
+            .into_future()
+            .map_err(|(err, _)| err)
+            .and_then(|(client, _)| client.unwrap().0)
+            .map(|client| Builder::new().new_read(client))
+            .and_then(|client| {
+                client
+                    .into_future()
+                    .map_err(|(err, _)| err)
+                    .map(|(msg, _)| msg)
+            })
+            .and_then(|msg| {
+                let msg = msg.unwrap();
+                assert_eq!(msg, data);
+                Ok(())
+            });
+
+        let dialer_side = transport
+            .clone()
+            .dial(addr)
+            .unwrap()
+            .map(|server| Builder::new().new_write(server))
+            .and_then(|server| server.send(data.into()))
+            .map(|_| ());
+
+        let combined = listener_side.select(dialer_side)
+            .map_err(|(err, _)| panic!("{:?}", err))
+            .map(|_| ());
+        let mut rt = Runtime::new().unwrap();
+        rt.block_on(combined).unwrap();
+    })
 }
 
 #[bench]
-fn connect_and_send_one_kb(bench: &mut test::Bencher) {
+fn mplex_connect_and_send_hello(bench: &mut test::Bencher) {
+    mplex_connect_and_send_data(bench, b"hello world")
+}
+
+#[bench]
+fn raw_tcp_connect_and_send_hello(bench: &mut test::Bencher) {
+    raw_tcp_connect_and_send_data(bench, b"hello world")
+}
+
+#[bench]
+fn mplex_connect_and_send_one_kb(bench: &mut test::Bencher) {
     let data = (0 .. 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
-    connect_and_send_data(bench, &data)
+    mplex_connect_and_send_data(bench, &data)
 }
 
 #[bench]
-fn connect_and_send_one_mb(bench: &mut test::Bencher) {
+fn raw_tcp_connect_and_send_one_kb(bench: &mut test::Bencher) {
+    let data = (0 .. 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
+    raw_tcp_connect_and_send_data(bench, &data)
+}
+
+#[bench]
+fn mplex_connect_and_send_one_mb(bench: &mut test::Bencher) {
     let data = (0 .. 1024 * 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
-    connect_and_send_data(bench, &data)
+    mplex_connect_and_send_data(bench, &data)
 }
 
 #[bench]
-fn connect_and_send_two_mb(bench: &mut test::Bencher) {
+fn raw_tcp_connect_and_send_one_mb(bench: &mut test::Bencher) {
+    let data = (0 .. 1024 * 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
+    raw_tcp_connect_and_send_data(bench, &data)
+}
+
+#[bench]
+fn mplex_connect_and_send_two_mb(bench: &mut test::Bencher) {
     let data = (0 .. 2 * 1024 * 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
-    connect_and_send_data(bench, &data)
+    mplex_connect_and_send_data(bench, &data)
+}
+
+#[bench]
+fn raw_tcp_connect_and_send_two_mb(bench: &mut test::Bencher) {
+    let data = (0 .. 2 * 1024 * 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
+    raw_tcp_connect_and_send_data(bench, &data)
 }
